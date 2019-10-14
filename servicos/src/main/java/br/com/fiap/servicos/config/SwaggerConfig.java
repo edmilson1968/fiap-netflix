c7622338e33fc5b521355de7e617c7c9f137d4a7
Edmilson Figueiredo <edmilson.figueiredo@gmail.com>
2019-10-13 22:25:18 -0300

    acerta package de serviços

--- servicos/src/main/java/br/com/fiap/servicos/config/SwaggerConfig.java
+++ servicos/src/main/java/br/com/fiap/servicos/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.fiap.clientes.controller"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.fiap.servicos.controller"))
                 .paths(PathSelectors.any()).build().pathMapping("/")
                 ;
     }
